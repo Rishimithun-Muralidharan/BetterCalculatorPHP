29169799bdbaa43631b007af068ad643abf9da37
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-03-09 13:23:13 +0530

    first commit

---
+++ bettercalculator.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <title>Better Calculator</title>
+    <meta charset="UTF-8">
+
+  </head>
+  <body>
+    <?php
+    
+    ?>
+
+  </body>
+  
+</html>
