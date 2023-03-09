5885578a837dec03d60b73829f03d5918265a2f9
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-03-09 13:38:18 +0530

    second commit

--- bettercalculator.php
+++ bettercalculator.php
@@ -6,8 +6,17 @@
 
   </head>
   <body>
+    <form action="bettercalculator.php" method="post">
+      <p>What is the first number</p><input type="number" name="number1">
+      <br>
+      <p>What is the second number</p><input type="number" name="number2">
+      <br>
+      <p><input type="submit"></p>
+
+    </form>
     <?php
-    
+
+
     ?>
 
   </body>
